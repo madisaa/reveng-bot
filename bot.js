539bb6321398d2c6cf62f85b48d20264409c4122
madisaa <43490282+madisaa@users.noreply.github.com>
2018-09-22 12:26:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -300,7 +300,7 @@ client.on('message',  message => {
   };
 });
    client.on('message', message => {
-     if (message.content === "*id") {
+     if (message.content === "#id") {
      let embed = new Discord.RichEmbed()
   .setThumbnail(message.author.avatarURL)
   .setAuthor(message.author.username)
@@ -318,6 +318,40 @@ client.on('message',  message => {
     }
 });
 
+const fs = require("fs");
+client.on('message', async message =>{
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let xp = require("./xp.json");
 
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+});
 
 client.login(process.env.BOT_TOKEN);
