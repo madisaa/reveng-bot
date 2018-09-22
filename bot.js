da9858749f084165c8781e20610574382d6b9d55
madisaa <43490282+madisaa@users.noreply.github.com>
2018-09-22 10:20:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,5 +69,44 @@ client.on('message', message => {
   message.channel.send({embed});
   }
 });
+const bannedwords = [//Alpha Codes
+  "#credit",//Alpha Codes
+  "#profile",//Alpha Codes
+//Alpha Codes
+  "#rep",//Alpha Codes
+  "#top",//Alpha Codes
+  "%level",//Alpha Codes
+  "%تقديم",//Alpha Codes
+  "-play",//Alpha Codes
+  "-stop",//Alpha Codes
+  "-p",//Alpha Codes
+  "-s",//Alpha Codes
+  "!invites",//Alpha Codes
+  "!top",//Alpha Codes
+  "G.play",//Alpha Codes
+  "G.stop",
+  "G.skip",
+  "-skip"//Alpha Codes
+//Alpha Codes
+
+]
+client.on('message', message => {
+  var Muted = message.guild.roles.find("name", "muted");
+  var warn = message.guild.roles.find("name", "warn");
+  if(bannedwords.some(word => message.content.includes(word))) {
+  if(message.channel.id !== 'id chat') return;
+  if (message.author.bot) return;
+  if(message.member.roles.has(warn)) return;
+  if(!message.member.roles.has(warn.id)) {
+  message.member.addRole(warn)
+  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
+  }
+  if(message.member.roles.has(warn.id)) {
+      message.member.addRole(Muted)
+      message.member.removeRole(warn)
+      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
+  }
+  }
+  })
 
 client.login(process.env.BOT_TOKEN);
