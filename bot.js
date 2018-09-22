10593b4b3bf47330f38124b2c1aaf39dadc52d62
madisaa <43490282+madisaa@users.noreply.github.com>
2018-09-22 12:43:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`! Gravity`,"http://twitch.tv/S-F")
+client.user.setGame(`! Gravity 4 Ever htpps://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
